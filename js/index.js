b596ebe7abb049f71666309807d7c702294e8eaa
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2024-10-29 11:37:26 +0100

    Create parti todo html bootstrap

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let todo = document.getElementById("todo")
+
+
+
+function ajoute() {
+    
+   
+}
+
+
+function afech() {
+    
+        todo.innerHTML =
+        `
+                  <div class="card-body border border-3  border-danger  leftr m-2 ">
+            <h5 class="card-title">Brief Soutenances Croisées</h5>
+            <div>
+              <button type="button" class="btn btn-primary">Edit</button>
+              <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+          </div>
+          <div class="card-body border border-3 border-warning leftj  m-2 ">
+            <h5 class="card-title">Brief Soutenances Croisées</h5>
+            <div>
+              <button type="button" class="btn btn-primary">Edit</button>
+              <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+          </div>
+          <div class="card-body border border-3 border-success leftg m-2 ">
+            <h5 class="card-title">Brief Soutenances Croisées</h5>
+            <div>
+              <button type="button" class="btn btn-primary">Edit</button>
+              <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+          </div>
+
+        `  
+}
+
+
+
+afech()
